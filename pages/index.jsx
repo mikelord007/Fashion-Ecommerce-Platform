b53c5226614bd8e4da9ca8d8aa4977ea97a49f29
manu jasan <manujasan23@gmail.com>
2022-10-20 19:15:21 +0530

    animation done partiallY

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { IoIosArrowForward } from 'react-icons/io';
 import ImageWrapper from '../components/ImageWrapper'
+import Navbar from '../components/Navbar';
+import { gsap } from "gsap";
+import styles from '../styles/Home.module.css'
+
+//assets
 import ModelPic1 from '../public/assets/images/model1.png'
 import ModelPic2 from '../public/assets/images/model2.png'
 import ModelPic3 from '../public/assets/images/model3.png'
@@ -13,12 +18,26 @@ import hollowcircle from '../public/assets/svgs/hollowcircle.svg'
 import modelcirclemain from '../public/assets/svgs/modelcirclemain.svg'
 import modelcirclesecondary from '../public/assets/svgs/modelcirclesecondary.svg'
 import ribbon from '../public/assets/svgs/ribbon.svg'
-import Navbar from '../components/Navbar';
-import styles from '../styles/Home.module.css'
+
 
 const Home = () => {
 
   const [activeModelPic, setactiveModelPic] = useState(1)
+
+  useEffect(() => {
+    setTimeout(() => {
+      var tl = gsap.timeline();
+      tl.to('.stagger_slide_up_title', { duration: 0.4, y: '-11rem', stagger: { amount: 0.3}})
+      .fromTo('.stagger_slide_up',{ y: '6rem', opacity: 0.5 }, {duration: 0.4, display: 'inherit',y: '0rem', opacity: 1, stagger: { amount: 0.3} })
+      .fromTo('.model_carousel_animation', { x: '7rem' }, {duration: 0.3, x: '0rem', stagger: { amount: 0.3}})
+      .fromTo('.image_shrink_animation',{scale: '1.5'}, {scale: '1',duration: 0.3, stagger: { amount: 0.3}, onComplete: function () {
+        gsap.set(this.targets(), { clearProps: "all" });
+      }}, '-=0.9')
+    },2000)
+  
+    
+  }, [])
+  
 
   return (
     <>
@@ -28,21 +47,27 @@ const Home = () => {
             <div className={styles.productpage__left}>
               <div className={styles.productpage__left__info}>
                 <h1 className={styles.productpage__left__info__title}>
-                  TIED GREEN <br/> V-NECK SHIRT
+                  <span className={['newline', 'stagger_slide_up_title', 'stagger_slide_up_title_1'].join(' ')}>TIED GREEN</span>
+                  <span className={['newline', 'stagger_slide_up_title', 'stagger_slide_up_title_2'].join(' ')}>V-NECK SHIRT</span>
                 </h1>
-                <h2 className={styles.productpage__left__info__price}>
+                <h2 className={[styles.productpage__left__info__price, 'stagger_slide_up'].join(' ')}>
                   $110
                 </h2>
                 <p className={styles.productpage__left__info__desc}>
-                  V-neck shirt with lapel collar. Long sleeves with cuffs. Front tie at hem. Front button closure.
+                  <span className={['newline', 'stagger_slide_up'].join(' ')}>
+                    V-neck shirt with lapel collar. Long sleeves with cuffs.
+                  </span>
+                  <span className={['newline', 'stagger_slide_up'].join(' ')}>
+                    Front tie at hem. Front button closure.
+                  </span>
                 </p>
                 <button className={styles.productpage__left__info__addbtn}>ADD</button>
               </div>
               <div className={styles.productpage__left__size}>
-                <h2 className={styles.productpage__left__size__title}>
+                <h2 className={[styles.productpage__left__size__title, 'stagger_slide_up'].join(' ')}>
                   Select Size
                 </h2>
-                <div className={styles.productpage__left__size__select}>
+                <div className={[styles.productpage__left__size__select, 'stagger_slide_up'].join(' ')}>
                   <button className={styles.productpage__left__size__select__btn}>S</button>
                   <button className={styles.productpage__left__size__select__btn}>M</button>
                   <button className={styles.productpage__left__size__select__btn}>L</button>
@@ -50,17 +75,17 @@ const Home = () => {
                 </div>
               </div>
               <div className={styles.productpage__left__carousel} >
-                <div className={styles.productpage__left__carousel__imgcontainer} >
-                  <button onClick={() => setactiveModelPic(1)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img1].join(' ')} />
+                <div className={[styles.productpage__left__carousel__imgcontainer, 'model_carousel_animation'].join(' ')} >
+                  <button onClick={() => setactiveModelPic(1)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img1, 'image_shrink_animation'].join(' ')} />
                 </div>
-                <div className={styles.productpage__left__carousel__imgcontainer} >
-                  <button onClick={() => setactiveModelPic(2)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img2].join(' ')} />
+                <div className={[styles.productpage__left__carousel__imgcontainer, 'model_carousel_animation'].join(' ')} >
+                  <button onClick={() => setactiveModelPic(2)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img2, 'image_shrink_animation'].join(' ')} />
                 </div>
-                <div className={styles.productpage__left__carousel__imgcontainer} >
-                  <button onClick={() => setactiveModelPic(3)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img3].join(' ')} />
+                <div className={[styles.productpage__left__carousel__imgcontainer, 'model_carousel_animation'].join(' ')} >
+                  <button onClick={() => setactiveModelPic(3)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img3, 'image_shrink_animation'].join(' ')} />
                 </div>
-                <div className={styles.productpage__left__carousel__imgcontainer} >
-                  <button onClick={() => setactiveModelPic(4)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img4].join(' ')} />
+                <div className={[styles.productpage__left__carousel__imgcontainer, 'model_carousel_animation'].join(' ')} >
+                  <button onClick={() => setactiveModelPic(4)} className={[styles.productpage__left__carousel__imgcontainer__img, styles.productpage__left__carousel__imgcontainer__img4, 'image_shrink_animation'].join(' ')} />
                 </div>
                 <button className={styles.productpage__left__carousel__morebtn}>
                   <IoIosArrowForward className={styles.productpage__left__carousel__morebtn__icon}/>
